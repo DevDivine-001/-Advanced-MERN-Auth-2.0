53376d70b92470eccf0c14dc6dcf5171bb24ec07
Oscdivin <divineuwandu84@Gmail.com>
2024-12-03 16:11:17 +0100

    app file is ready to export to other file be use

---
+++ backend/src/app.ts
@@ -0,0 +1,34 @@
+import "dotenv/config" // Load environment variables first
+import cookieParser from "cookie-parser"
+import express, { Request, Response } from "express"
+import cors from "cors"
+import { config } from "./config/app.config"
+import connectDatabase from "./DB/Database"
+
+
+const app = express()
+// const BASE_PATH = config.BASE_PATH
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+    origin: config.APP_ORIGIN,
+    credentials: true
+}))
+
+app.use(cookieParser())
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Hello Subscribers!!!!👋👋👋👋"
+
+    })
+})
+
+app.listen(config.PORT, async () => {
+    console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`)
+    await connectDatabase()
+})
+
