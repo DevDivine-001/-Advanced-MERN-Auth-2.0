d3427cb3bd20197e73e2eec85ce09e5d9a73132a
Oscdivin <divineuwandu84@Gmail.com>
2024-12-04 08:31:15 +0100

    Added errorhandler file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import express, { Request, Response } from "express"
 import cors from "cors"
 import { config } from "./config/app.config"
 import connectDatabase from "./DB/Database"
+import { errorHandler } from "./middlewares/errorHandler"
 
 
 const app = express()
@@ -26,6 +27,8 @@ app.get("/", (req: Request, res: Response) => {
 
     })
 })
+
+app.use(errorHandler)
 
 app.listen(config.PORT, async () => {
     console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`)
---
+++ backend/src/middlewares/errorHandler.ts
@@ -0,0 +1,16 @@
+import { ErrorRequestHandler } from "express";
+
+export const errorHandler: ErrorRequestHandler = (error, req, res): any => {
+    console.log(`Error Occurred on PATH: ${req.path}`, error)
+
+    if (error instanceof SyntaxError) {
+        return res.status(400).json({
+            message: "Invalid JSON format, place check your request body"
+        })
+    }
+
+    return res.status(500).json({
+        message: "Internal server Error",
+        error: error?.message || "UnKnown error occurred"
+    })
+} 
