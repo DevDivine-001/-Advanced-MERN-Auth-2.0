c35351d1d4bb4f49c5993adae3f55f7c58e62abe
Oscdivin <divineuwandu84@Gmail.com>
2024-12-04 10:13:19 +0100

    put http request im in slide'

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,15 +1,16 @@
 import { ErrorRequestHandler } from "express";
+import { HTTPSTATUS } from "../config/http.config";
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next): any => {
     console.error(`Error Occurred on PATH: ${req.path}`, error)
 
     if (error instanceof SyntaxError) {
-        return res.status(400).json({
+        return res.status(HTTPSTATUS.BAD_REQUEST).json({
             message: "Invalid JSON format, place check your request body"
         })
     }
 
-    return res.status(500).json({
+    return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
         message: "Internal server Error",
         error: error?.message || "UnKnown error occurred"
     })
